72602837275f46c6bcfa3af07d81a5bd01e53447
dusandj <dusan.djukic@ematicsolutions.com>
2019-09-13 08:57:26 +0200

    add turn server

--- server/public/client.js
+++ server/public/client.js
@@ -98,7 +98,12 @@ let rtcPeerConnection, dataChannel, receiveChannel;
 const iceServers = {
     'iceServers': [
         {'urls': 'stun:stun.services.mozilla.com'},
-        {'urls': 'stun:stun.l.google.com:19302'}
+        {'urls': 'stun:stun.l.google.com:19302'},
+        {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        }
     ]
 }
 const streamConstraints = {
