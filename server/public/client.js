61cc4a08028177dd30a135691755785bf0e5fd0f
dusandj <dusan.djukic@ematicsolutions.com>
2019-09-13 09:01:21 +0200

    url to urls

--- server/public/client.js
+++ server/public/client.js
@@ -100,7 +100,7 @@ const iceServers = {
         {'urls': 'stun:stun.services.mozilla.com'},
         {'urls': 'stun:stun.l.google.com:19302'},
         {
-            url: 'turn:192.158.29.39:3478?transport=udp',
+            urls: 'turn:192.158.29.39:3478?transport=udp',
             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             username: '28224511:1379330808'
         }
